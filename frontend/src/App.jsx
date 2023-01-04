2f33165d7077f77149422019aa7cb0f738dcd780
Cesar Nascimento <cnascimento@dermage.com.br>
2023-01-04 14:07:00 -0300

    Dermage Search v0.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   <>
   <Box bg='white' w='100vw'>
     <Heading bg='#ff8400'>
-      <Image src='src/assets/logo.png' alt='dermage-logo' bg='#ff8400'></Image>
+      <Image src='public/assets/logo.png' alt='dermage-logo' bg='#ff8400'></Image>
     </Heading>
       {<ListaProdutos/>}
       {/* {<Teste/>} */}
