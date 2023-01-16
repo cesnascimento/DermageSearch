f68dc949473036b8e77192811540eb2a6680224d
Cesar Nascimento <cnascimento@dermage.com.br>
2023-01-16 11:13:49 -0300

    Adicionado scraper da loja PANVEL

---
+++ scrapers/drogariavenancio/drogariavenancio.py
@@ -0,0 +1,56 @@
+import requests
+import locale
+import json
+from ulid import ULID
+from datetime import datetime
+from headers_drogariavenancio import headers
+
+
+drogariacatarinense_session = requests.Session()
+DICIO = {}
+locale.setlocale(locale.LC_MONETARY, '')
+
+
+def requisicao_drogariacatarinense():
+    drogariacatarinense = drogariacatarinense_session.get(
+        'https://www.drogariavenancio.com.br/api/catalog_system/pub/products/search?&ft=dermage&PS=24&sl=59e923b8-8598-4b5b-9e5d-e9fef0d71623&cc=24&sm=0&PageNumber=1&_from=0&_to=49', headers=headers).json()
+    lista_produtos = [produto for produto in drogariacatarinense]
+    return lista_produtos
+
+
+def buscar_produtos(lista_items):
+    links_produtos = [produto['link'] for produto in lista_items]
+    lista_produtos = [produto['items'] for produto in lista_items]
+    print(lista_produtos, links_produtos)
+    return lista_produtos, links_produtos
+
+
+def data_hora():
+    data = datetime.now()
+    atual = data.strftime("%d/%m/%Y - %H:%M:%S")
+    return atual
+
+
+def informacoes_produtos():
+    info_produtos, links_produtos = buscar_produtos(requisicao_drogariacatarinense())
+    DICIO['precos'] = []
+    DICIO['lojas'] = [{'id': 8, 'nome': 'Drogaria Venancio',
+                      'site': 'https://www.drogariavenancio.com.br'}]
+    for produto, link in zip(info_produtos, links_produtos):
+        ean, preco = produto[0]['ean'], locale.currency(
+            produto[0]['sellers'][0]['commertialOffer']['Price'])
+        DICIO['precos'].append(
+            {'id': str(ULID()), 'ean_id': ean, 'loja_id': 2, 'preco': preco, 'link': link, 'datahora': data_hora()})
+    return DICIO
+
+
+def criar_json(info):
+    with open('JSON/drogariacatarinense.json', 'w') as jsonfile:
+        json.dump(info, jsonfile)
+
+
+def start():
+    criar_json(informacoes_produtos())
+
+
+start()
---
+++ scrapers/panvel/panvel.py
@@ -0,0 +1,58 @@
+import requests
+import locale
+import json
+from datetime import datetime
+from ulid import ULID
+from headers_panvel import headers, json_data, cookies
+
+panvel_session = requests.Session()
+DICIO = {}
+locale.setlocale(locale.LC_MONETARY, '')
+
+
+def requisicao_panvel():
+    response = [panvel_session.post('https://www.panvel.com/api/v2/search',
+                                    cookies=cookies, headers=headers, json=json_data(page)).json()['items'] for page in range(1, 5)]
+    return response
+
+
+def navegar_panvel(produtos):
+    info = [info for produto in produtos for info in produto]
+    return info
+
+
+def data_hora():
+    data = datetime.now()
+    atual = data.strftime("%d/%m/%Y - %H:%M:%S")
+    return atual
+
+
+def informacao_produto(infos):
+    DICIO['precos'] = []
+    DICIO['lojas'] = [
+        {'id': 8, 'nome': 'Panvel', 'site': 'https://www.panvel.com'}]
+    for info in infos:
+        panvel_code = info['panvelCode']
+        panvel = panvel_session.get(
+            f'https://www.panvel.com/api/v2/catalog/{panvel_code}?uf=RS', cookies=cookies, headers=headers).json()
+        ean, preco = panvel['ean'], locale.currency(
+            panvel['discount']['dealPrice'])
+        DICIO['precos'].append(
+            {'id': str(ULID()), 'ean_id': ean, 'loja_id': 1, 'preco': preco, 'datahora': data_hora()})
+    
+    return DICIO
+
+
+def criar_json(info):
+    with open('../JSON/panvel.json', 'w') as jsonfile:
+        json.dump(info, jsonfile)
+
+
+def start():
+    criar_json(informacao_produto(navegar_panvel(requisicao_panvel())))
+
+
+start()
+
+
+# api: https://www.panvel.com/api/v2/catalog/601010?uf=RS
