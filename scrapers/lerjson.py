efb753c1228c1dc6e4ce88bc74e53dc07e0fa0e4
Cesar Nascimento <cnascimento@dermage.com.br>
2023-01-24 16:44:38 -0300

    Adicionado a função de alerta de email dos preços

--- scrapers/lerjson.py
+++ scrapers/lerjson.py
@@ -1,5 +1,6 @@
 import json
 import os
+from O365 import Account, Connection, FileSystemTokenBackend
 
 
 def folder_jsons():
@@ -23,6 +24,67 @@ def get_json_geral(files):
     return file
 
 
+def alert_price(dermage, files):
+    for json_file in files:
+        with open(f'{json_file}', 'r') as file:
+            for f in file:
+                print(file)
+                archives = json.loads(f)
+                for name, price_dermage in zip(dermage['produtos'], dermage['precos']):
+                    for price_file in archives['precos']:
+                        if price_dermage['ean_id'] == price_file['ean_id']:
+                            a,b = price_dermage['preco'].replace('R$','').replace(',', '.'), price_file['preco'].replace('R$','').replace(',', '.')
+                            if abs(float(a) - (float(a) * 0.15)) >= float(b):
+                                try:
+                                    difference = abs(float(a) - float(b))
+                                    percent = (difference / float(b)) * 100
+                                    send_email_alert(name['nome'], price_file['link'], b, "{:.1f}%".format(percent))
+                                except:
+                                    pass
+
+
+def refresh_token():
+    credentials = ('3ba13a6c-e200-4e7c-ace1-b17f47f410df',
+                   '_lr8Q~-6NPRbD2CU4zszbZMfMkrki7IE5zRKCbau')
+    token_backend = FileSystemTokenBackend(
+        token_path=os.path.join(os.getcwd(), 'Conf_app'), token_filename='o365_token.txt'
+    )
+    account = Account(credentials, token_backend=token_backend)
+    if not account.is_authenticated:
+        account.authenticate(scopes=['basic', 'message_all'])
+
+    connection = Connection(credentials, token_backend=token_backend, scopes=[
+                            'basic', 'message_all'])
+    connection.refresh_token()
+
+    print("Outlook autenticado.")
+    return account
+
+def send_email_alert(produto, loja, valor, porcentagem):
+    loja2 = loja.split('.')[1]
+    account = refresh_token()
+    if account.is_authenticated == True:
+        m = account.new_message()
+        m.to.add('cowlfnt@gmail.com')
+        m.subject = f'Alerta de preço – {loja2} / R${valor} / {porcentagem}'
+        body = f"""
+            <html>
+                <body>
+                    <center><h1>Relatório do Robô Dermage Product Search</h1></center>
+                    <p>Produtos: {produto}</p>
+                    <p>Loja: {loja2}</p>
+                    <p>Link: {loja}</p>
+                    <p>Valor: R${valor}</p>
+                    <p>Porcentagem: {porcentagem}</p>
+                </body>
+            </html>"""
+        m.body = body
+        m.send()
+        print('Email Enviado')
+    else:
+        print('Outlook não conectado')
+
+
 def build_json(dermage, jsons):
     for json in jsons:
         archive = open_archive_json(json)
@@ -43,6 +105,7 @@ def start():
     dermage = open_archive_json(file_dermage)
     jsons = get_json_geral(folder_jsons())
     file = build_json(dermage, jsons)
+    alert_price(dermage, jsons)
     create_json(file)
 
 
